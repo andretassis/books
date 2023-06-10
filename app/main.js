1682541ac7b8f0f78b9eb19f689577226b7b74e8
Andre Tassis <124065777+andretassis@users.noreply.github.com>
2023-06-10 20:01:30 -0300

    Add files via upload
    Criada função de aplicar descontos com o .map

--- app/main.js
+++ app/main.js
@@ -8,9 +8,11 @@ async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI);
     livros = await res.json();
 
-    exibirLivros(livros);
+    let livrosComDesconto = aplicarDesconto(livros);
+    exibirLivros(livrosComDesconto);
 }
 
+//exibe a lista de livros com suas características na tela
 function exibirLivros(listaDeLivros) {
     listaDeLivros.forEach((livro) => {
         exibicaoLivros.innerHTML += `
@@ -20,10 +22,20 @@ function exibirLivros(listaDeLivros) {
         ${livro.titulo}
         </h2>
         <p class="livro__descricao">${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
+        <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
         <div class="tags">
         <span class="tag">${livro.categoria}</span>
         </div>
         `
     });
 }
+
+//aplica o desconto de no valor do livro
+function aplicarDesconto(livros) {
+    const desconto = 0.3
+    livrosComDesconto = livros.map(livro => {
+        return { ...livro, preco: livro.preco - (livro.preco * desconto) }
+    })
+    return livrosComDesconto;
+}
+
