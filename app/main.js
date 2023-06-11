cbf10779fa5c92779db5f99651a331cbb39efd9e
Andre Tassis <124065777+andretassis@users.noreply.github.com>
2023-06-11 19:12:04 -0300

    Add files via upload
    Criada a manipulação dos botões para filtrar de acordo com o tipo de categoria clicada.

--- app/main.js
+++ app/main.js
@@ -1,19 +1,22 @@
-let livros = []
-const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
-const exibicaoLivros = document.getElementById('livros')
+let livros = [] // variavel livros com array vazio
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json' // api de onde os dados são extraídos
+const exibicaoLivros = document.getElementById('livros') // encontra a section do html para exibição dinâmica dos livros
 
 getBuscarLivrosDaAPI()
 
+//faz o fetch da api, converte os dados para json e armazena na variavel livros
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI);
     livros = await res.json();
 
+    //aplica o desconto e exibe os livros com desconto
     let livrosComDesconto = aplicarDesconto(livros);
     exibirLivros(livrosComDesconto);
 }
 
 //exibe a lista de livros com suas características na tela
 function exibirLivros(listaDeLivros) {
+    exibicaoLivros.innerHTML = ''
     listaDeLivros.forEach((livro) => {
         exibicaoLivros.innerHTML += `
         <div class="livro">
@@ -30,7 +33,7 @@ function exibirLivros(listaDeLivros) {
     });
 }
 
-//aplica o desconto de no valor do livro
+//aplica o desconto de no valor do livro percorrendo todo o array mantendo todas as caracteristicas originais e alterando apenas o preco
 function aplicarDesconto(livros) {
     const desconto = 0.3
     livrosComDesconto = livros.map(livro => {
